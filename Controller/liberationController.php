47e293e33b6086e044e553e999183bb2e9fc3e5a
bbergstorm <benoit@myskreen.com>
2013-04-29 11:12:54 +0200

    link seo

--- Controller/liberationController.php
+++ Controller/liberationController.php
@@ -26,22 +26,22 @@ class LiberationController extends Controller
     {
       $api = new ApiManager($this->container->getParameter('kernel.environment'), '.json', 2);
       $data = $api->fetch('www/home/tv-replay', array(
-				'without_footer' => true,
-				'with_programs' => true,
-				'img_width' => 160,
-				'img_height' => 200,
-				'with_teaser' => true,
-				'with_pass' => true,
-				'slider_width' => 990
-			));
+        'without_footer' => true,
+        'with_programs' => true,
+        'img_width' => 160,
+        'img_height' => 200,
+        'with_teaser' => true,
+        'with_pass' => true,
+        'slider_width' => 990
+      ));
      //echo $api->url;
      //print_r($datas);
 
-			$page = file_get_contents('http://www.liberation.fr/partenaires/100/');
-			$page = str_replace('<!-- contenu partenaire ici -->', '{% block content endblock %}', $page);
-			$fichier = fopen(dirname(__FILE__) . '/../Resources/views/liberation.html.twig','w+');
-			fputs($fichier, $page);
-			fclose($fichier);
+      $page = file_get_contents('http://www.liberation.fr/partenaires/100/');
+      $page = str_replace('<!-- contenu partenaire ici -->', '{% block content endblock %}', $page);
+      $fichier = fopen(dirname(__FILE__) . '/../Resources/views/liberation.html.twig','w+');
+      fputs($fichier, $page);
+      fclose($fichier);
 
       $response = $this->render('SkreenHouseFactoryPartnersBundle:Liberation:main.html.twig', array(
         'home' => $data,
