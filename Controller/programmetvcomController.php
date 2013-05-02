47a00ae3e0c61c6993723e0dbc1fbb7eb146bd4d
bbergstorm <benoit@myskreen.com>
2013-05-02 09:50:44 +0200

    programmtvcom

---
+++ Controller/programmetvcomController.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the SkreenHouseFactoryV3Bundle package.
+ *
+ * (c) Brickstorm <http://brickstorm.org/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SkreenHouseFactory\partnersBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use SkreenHouseFactory\v3Bundle\Api\ApiManager;
+
+use \DOMDocument;
+use \DOMXPath;
+
+class ProgrammetvcomController extends Controller
+{
+    /**
+    * homes
+    */
+    public function mainAction(Request $request)
+    {
+      $api = new ApiManager($this->container->getParameter('kernel.environment'), '.json', 2);
+      $data = $api->fetch('www/home/tv-replay', array(
+        'without_footer' => true,
+        'with_programs' => true,
+        'img_width' => 160,
+        'img_height' => 200,
+        'with_teaser' => true,
+        'with_pass' => true,
+        'slider_width' => 990
+      ));
+     //echo $api->url;
+     //print_r($datas);
+
+      //$page = utf8_encode(file_get_contents('http://www.programme-tv.com/'));
+      //$page = preg_replace('/<div id="content">(.*?)<\/div>/i', '<div id="content">{% block content endblock %}</div>', $page);
+
+      $dom = new DOMDocument;
+      libxml_use_internal_errors(TRUE);
+      $dom->loadHTMLFile('http://www.programme-tv.com/');
+      libxml_clear_errors();
+
+      $xp = new DOMXPath($dom);
+
+      // create the new element
+      $newNode = $dom->createElement('div', '{% block content endblock %}');
+      $newNode->setAttribute('id', 'content');
+
+      // fetch and replace the old element
+      $oldNode = $dom->getElementById('content');
+      $oldNode->parentNode->replaceChild($newNode, $oldNode);
+
+      // print xml
+      $page = $dom->saveXml($dom->documentElement);
+
+      $page = str_ireplace('charset=iso-8859-1','charset=utf-8',$page);
+      $fichier = fopen(dirname(__FILE__) . '/../Resources/views/programmetvcom.html.twig','w+');
+      fputs($fichier, $page);
+      fclose($fichier);
+
+      $response = $this->render('SkreenHouseFactoryPartnersBundle:ProgrammeTvCom:main.html.twig', array(
+        'home' => $data,
+      ));
+
+      $maxage = 300;
+      $response->setPublic();
+      $response->setMaxAge($maxage);
+      $response->setSharedMaxAge($maxage);
+      
+      return $response;
+    }
+}
