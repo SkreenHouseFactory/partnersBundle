ffb1f05ac8b9e22d2d81cb12238d85768f5a0b40
bbergstorm <benoit@myskreen.com>
2013-05-16 16:45:08 +0200

    cdata script

--- Controller/directmatinController.php
+++ Controller/directmatinController.php
@@ -29,7 +29,7 @@ class DirectmatinController extends Controller
   {
     $layout = dirname(__FILE__) . '/../Resources/views/directmatin.html.twig';
     @unlink($layout);
-    
+
     $api = new ApiManager($this->container->getParameter('kernel.environment'), '.json', 2);
     $data = $api->fetch('www/home/tv-replay', array(
       'without_footer' => true,
@@ -43,15 +43,10 @@ class DirectmatinController extends Controller
    //echo $api->url;
    //print_r($datas);
 
-    //$page = utf8_encode(file_get_contents('http://www.programme-tv.com/'));
-    //$page = preg_replace('/<div id="content">(.*?)<\/div>/i', '<div id="content">{% block content endblock %}</div>', $page);
-
     $dom = new DOMDocument;
     libxml_use_internal_errors(TRUE);
     $dom->loadHTMLFile('http://www.directmatin.fr/myskreen');
     libxml_clear_errors();
-
-    $xp = new DOMXPath($dom);
 
     // create the new element
     $newNode = $dom->createElement('div', '{% block content endblock %}');
@@ -64,7 +59,20 @@ class DirectmatinController extends Controller
     // print xml
     $page = $dom->saveXml($dom->documentElement);
 
-    $page = str_ireplace(array('charset=iso-8859-1', '<![CDATA[', ']]>'),array('charset=utf-8', ''),$page);
+    $page = preg_replace('/^\s*\/\/<!\[CDATA\[([\s\S]*)\/\/\]\]>\s*\z/', 
+                            '$1', 
+                            $page);
+
+    $page = str_ireplace(
+      array(
+        'charset=iso-8859-1', 
+        '<![CDATA[', 
+        ']]>'), 
+       array(
+        'charset=utf-8', 
+        '', 
+        ''), 
+      $page);
 
     $fichier = fopen($layout,'w+');
     fputs($fichier, $page);
