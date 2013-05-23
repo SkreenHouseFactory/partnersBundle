c4ef788e1b61a5488e4748a4d12a34f22dfc11e2
bbergstorm <benoit@myskreen.com>
2013-05-23 12:17:29 +0200

    add rm cache/prod/twig liberation

--- Controller/liberationController.php
+++ Controller/liberationController.php
@@ -26,6 +26,7 @@ class LiberationController extends Controller
     {
       $layout = dirname(__FILE__) . '/../Resources/views/liberation.html.twig';
       @unlink($layout);
+      exec('rm -rf '.$this->get('kernel')->getRootDir().'app/cache/prod/twig/');
 
       $api = new ApiManager($this->container->getParameter('kernel.environment'), '.json', 2);
       $data = $api->fetch('www/home/tv-replay', array(
@@ -50,7 +51,7 @@ class LiberationController extends Controller
         'home' => $data,
       ));
 
-      $maxage = 300;
+      $maxage = 3600;
       $response->setPublic();
       $response->setMaxAge($maxage);
       $response->setSharedMaxAge($maxage);
